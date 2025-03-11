ecd6ff4a5d3569baf4d64fe9b2d0c93df4cd5096
Arpa <aarni.viljanen@gmail.com>
2025-03-11 09:44:39 +0200

    Made all the files

---
+++ demo/src/main/java/main/ChampionshipManager.java
@@ -0,0 +1,12 @@
+package main;
+
+public class ChampionshipManager {
+    
+    //This makes sure there is only one instance of this class
+    private static ChampionshipManager instance;
+
+    //Constructor must be private
+    private ChampionshipManager() {
+
+    }
+}
---
+++ demo/src/main/java/main/Driver.java
@@ -0,0 +1,41 @@
+package main;
+
+public class Driver {
+
+    //Variables
+    private String name;
+    private String country;
+    private int points;
+    private RallyCar car;
+
+    //Constructor
+    public Driver(String name, String country, RallyCar car){
+        this.name = name;
+        this.country = country;
+        this.car = car;
+    }
+
+    //Getters
+    public String getName(){
+        return name;
+    }
+    public String getCountry(){
+        return country;
+    }
+    public int getPoints(){
+        return points;
+    }
+    public RallyCar getCar(){
+        return car;
+    }
+
+    //Setter
+    public void setCar(RallyCar car){
+        this.car = car;
+    }
+
+    //Method to add points
+    public void addPoints(int points){
+
+    }
+}
